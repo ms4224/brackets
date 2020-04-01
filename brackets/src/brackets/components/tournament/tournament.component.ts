ae4c012cb70e738d6241e79b9334c64cca74b0ce
Matias <schakelmatias@gmail.com>
2020-04-01 17:11:07 +0900

    Fixed match centering bug

--- brackets/src/brackets/components/tournament/tournament.component.ts
+++ brackets/src/brackets/components/tournament/tournament.component.ts
@@ -19,7 +19,8 @@ export class TournamentComponent implements OnInit {
     const fakeContestants = ['mario', 'yoshi', 'link', 'DK', 'CFalcon', 'kirby', 'fox', 'pikachu', 'Ness', 'jiggly', 'luigi', 'bowser', 'peach', 'isaac', 'diddykong',
       'luffy', 'zoro', 'sanji', 'usopp', 'nami', 'franky', 'robin', 'chopper', 'brooke', 'mr.3', 'crocodile', 'mr.1', 'miss all-sunday',
       'gon', 'kilua', 'kurapika', 'leorio', 'alien', 'goku', 'gohan', 'piccolo', 'krillin', 'tien', 'yamcha', 'chaozu', 'master roshi', 'vegeta',
-      'frieza', 'cell', 'majin buu', 'gotenks', 'trunks', 'goten', 'android 13', 'android 11', 'android 17', 'android 18', 'nappa', 'raditz'
+      'frieza', 'cell', 'majin buu', 'gotenks', 'trunks', 'goten', 'android 13', 'android 11', 'android 17', 'android 18', 'nappa', 'raditz',
+      'luke', 'leia', 'darth vader', 'darth maul', 'darth sidious', 'han solo', 'chewbacca', 'c3po', 'r2d2', 'quigon', 'obiwan', 'mace windu', 'yoda'
     ]
     this.tournament = this.dataService['createTournamentData'](createContestantDataFromStringList(fakeContestants));
   }
